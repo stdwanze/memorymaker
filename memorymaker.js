d3fa0865ec5bd51dec83a6f974d0a677dd8124d4
Stefan Dienst <stdwanze@googlemail.com>
2014-07-13 14:18:48 +0200

    with input

--- memorymaker.js
+++ memorymaker.js
@@ -1,19 +1,72 @@
 var memorymaker = window.memorymaker || {}; ( function(memorymaker) {
 
 
+
+		memorymaker.VM = (function (){
+			
+			function vm (canvas)
+			{
+				this.display = ko.observable(true);
+				this.words = ko.observable();
+				this.controller = new memorymaker.Controller(canvas);
+				this.controller.setup(new CanvasKit.Point(100,200));
+				this.deleteContent = function ()
+				{
+					this.controller.deleteContent();
+				};
+				this.loadAndDisplay = function ()
+				{
+					var rawInput = this.words();
+					var elements = rawInput.split(/[\n|,|;|\t]/);
+					
+					elements.forEach(function (element){
+						element = element.trim();
+						if(element.length > 0)
+							this.controller.addCard(element);
+					}.bind(this));
+					
+					//this.display(false);
+					
+					this.controller.draw();
+				};
+			}
+			return vm;
+		}());
 		memorymaker.Card = (function (){
 			
 			function card (topleft, cardSize, contentstyle)
 			{
-			   	this.tlcontent = topleft.clone();
-			   	this.tllabel = topleft.clone(); this.tllabel.y = this.tllabel.y + cardSize.y /2;
+				var factor = 4;
+				var cardbasesize = cardSize.clone();
+				cardbasesize.x -= 2*factor;
+				cardbasesize.y -= 2*factor;
+				
+				var cardbasetl = topleft.clone();
+				cardbasetl.x += factor; 
+				cardbasetl.y += factor;
+				
+				
+				this.groundContent = topleft.clone();
+				this.groundMemory = topleft.clone();this.groundMemory.y +=cardSize.y /2;
+				
+				this.groundContentStyle = new CanvasKit.ContentStyle();
+				this.groundContentStyle.fill = false;
+				this.groundContentStyle.bordercolor = "lightgrey";
+				
+			   	this.tlcontent = cardbasetl.clone();
+			   	this.tllabel = cardbasetl.clone(); this.tllabel.y = this.tllabel.y + cardSize.y /2;
 				this.elementsize = cardSize.clone(); this.elementsize.y = cardSize.y /2;
+				this.cardsize = cardbasesize.clone(); this.cardsize.y = (cardbasesize.y /2)-factor;
+				
 				this.contentMemory = new CanvasKit.ContentStyle();
 				this.contentMemory.fill = false;
 				this.contentMemory.text = "memory";
 				
-				this.rectContent =  new CanvasKit.Rectangle(this.tlcontent, this.elementsize, contentstyle);
-				this.rectMemory =  new CanvasKit.Rectangle(this.tllabel, this.elementsize, this.contentMemory);
+				this.baseContent =  new CanvasKit.Rectangle(this.groundContent, this.elementsize, this.groundContentStyle);
+				this.baseMemory =  new CanvasKit.Rectangle(this.groundMemory, this.elementsize,this.groundContentStyle);
+				
+				this.rectContent =  new CanvasKit.Rectangle(this.tlcontent, this.cardsize, contentstyle);
+				this.rectMemory =  new CanvasKit.Rectangle(this.tllabel, this.cardsize, this.contentMemory);
 				
 				
 			}
@@ -21,6 +74,8 @@ var memorymaker = window.memorymaker || {}; ( function(memorymaker) {
 				
 				render : function (canvas,ctxt)
 				{
+					this.baseContent.render(canvas,ctxt);
+					this.baseMemory.render(canvas,ctxt);
 					this.rectContent.render(canvas,ctxt);
 					this.rectMemory.render(canvas,ctxt);
 				}
@@ -88,6 +143,11 @@ var memorymaker = window.memorymaker || {}; ( function(memorymaker) {
 						cs.fill = false;
 						cs.text = text;
 						return cs;
+					},
+					deleteContent: function ()
+					{
+						this.shapes = [];
+						this.ctxt.clearRect(0, 0, this.canvas.width, this.canvas.height);
 					}
 				};
 				return controller;
